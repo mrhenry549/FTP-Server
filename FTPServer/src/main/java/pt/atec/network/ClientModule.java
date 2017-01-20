11a0e04e9e87af4366a288abbb12df9cf7f58223
Henrique <Henrique@Henrique-PC>
2017-01-20 15:34:40 +0000

    Ver Alterações

--- FTPServer/src/main/java/pt/atec/network/ClientModule.java
+++ FTPServer/src/main/java/pt/atec/network/ClientModule.java
@@ -1,127 +1,50 @@
 package pt.atec.network;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.*;
+
+import java.net.*;
 
 public class ClientModule {
 
-    public final static int SOCKET = 21, FILE_SIZE = 6022386;
-    public final static String SERVER = "192.168.250.158";
-    
-    public String FILE_TO_RECEIVED;
-    
-    Scanner s = new Scanner(System.in);
-    String numFicheiro, lsinput;
-    
-    PrintWriter out, outfile;
-    BufferedReader in, infile;
-    String inLine, outLine;
-    Socket sock;
+    private final int PORTO = 9983;
+    private final String IP = "192.168.250.250";
 
     public ClientModule() throws IOException {
-        int bytesRead;
-        int current = 0;
-        FileOutputStream fos1 = null, fos2 = null;
-        BufferedOutputStream bos1 = null, bos2 = null;
+
+        Socket sock = new Socket(IP, PORTO);
+
+        DataInputStream din = new DataInputStream(sock.getInputStream());
+        DataOutputStream dout = new DataOutputStream(sock.getOutputStream());
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String msgin = "", msgout = "";
+
+        byte[] bytearray = new byte[6022386];
+
+        InputStream is = sock.getInputStream();
+
         try {
-            sock = new Socket(SERVER, SOCKET);
-            
-            System.out.println("Conectado!");
-            
-            //Receber o array de ficheiros ***********************
-            lsinput = new String();
-            lsinput = s.nextLine();
-            
-            if(lsinput == "ls"){
-                byte[] bytearray1 = new byte[FILE_SIZE];
-                InputStream is1 = sock.getInputStream();
-                fos1 = new FileOutputStream("C:/arrayfile.txt");
-                bos1 = new BufferedOutputStream(fos1);
-                bytesRead = is1.read(bytearray1, 0, bytearray1.length);
-                current = bytesRead;
 
-                do {
-                    bytesRead = is1.read(bytearray1, current, (bytearray1.length - current));
-                    if (bytesRead >= 0) {
-                        current += bytesRead;
-                    }
-                } while (bytesRead > -1);
+            msgout = br.readLine();
+            dout.writeUTF(msgout);
+            System.out.println("Introduza o numero do ficheiro");
+            msgin = din.readUTF();
+            System.out.println(msgin);
+            FileOutputStream fos = new FileOutputStream("C:/Users/Henrique/Downloads");
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+            int bytesread = is.read(bytearray, 0, bytearray.length);
+            bos.write(bytearray, 0, bytesread);
+            System.out.println("O ficheiro foi recebido com sucesso!");
+            bos.close();
+            sock.close();
 
-                bos1.write(bytearray1, 0, current);
-                bos1.flush();
-                System.out.println("Array recebido.");
+        } catch (Exception SVI) {
 
+            System.out.print(SVI);
 
-                System.out.println("Conteudo do server:");
-
-                infile = new BufferedReader(new FileReader("C:/arrayfile.txt"));
-
-                String line;
-                int i = 0;
-                ArrayList<String> nomeDosFicheiros = new ArrayList<>();
-                while((line = infile.readLine()) != null)
-                {
-                    System.out.println(i+". "+line);
-                    nomeDosFicheiros.add(line);
-                    i++;
-                }
-                infile.close();
-
-                System.out.println("Seleciona o número do ficheiro que quer transferir:");
-
-                numFicheiro = s.nextLine();
-
-                outfile = new PrintWriter(new FileWriter("C:/outputfile.txt"));
-                outfile.print(numFicheiro); 
-                outfile.close();
-
-                for(int n = 0; n < nomeDosFicheiros.size(); n++){
-                    if(n == Integer.parseInt(numFicheiro)){
-                        FILE_TO_RECEIVED = "C:/Users/Henrique/Downloads/"+nomeDosFicheiros.get(n);
-                    }
-                }
-
-                // receber o ficheiro ***********************
-                byte[] bytearray2 = new byte[FILE_SIZE];
-                InputStream is2 = sock.getInputStream();
-                fos2 = new FileOutputStream(FILE_TO_RECEIVED);
-                bos2 = new BufferedOutputStream(fos2);
-                bytesRead = is2.read(bytearray2, 0, bytearray2.length);
-                current = bytesRead;
-
-                do {
-                    bytesRead = is2.read(bytearray2, current, (bytearray2.length - current));
-                    if (bytesRead >= 0) {
-                        current += bytesRead;
-                    }
-                } while (bytesRead > -1);
-
-                bos2.write(bytearray2, 0, current);
-                bos2.flush();
-                System.out.println("Ficheiro " + FILE_TO_RECEIVED + " transferido (" + current + " bytes lidos)");
-            }
-        } finally {
-            if (fos2 != null && fos1 != null) {
-                fos2.close();
-                fos1.close();
-            }
-            if (bos2 != null && bos1 != null) {
-                bos2.close();
-                bos1.close();
-            }
-            if (sock != null) {
-                sock.close();
-            }
         }
+
     }
 
 }
--- FTPServer/src/main/java/pt/atec/network/ServerModule.java
+++ FTPServer/src/main/java/pt/atec/network/ServerModule.java
@@ -1,141 +1,75 @@
 package pt.atec.network;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+
+import java.net.*;
 
 public final class ServerModule {
 
-    public final static int SOCKET = 21;
-    int numero = 0, i = 0;
-    String inputLine;
-
-    PrintWriter out, outfile;
-    BufferedReader in;
-
-    public String FILE_TO_SEND, LOG_FILE = "C:/ftp/logs.txt";
+    private final int PORTO = 9983;
 
     public ServerModule() throws IOException {
-        int bytesRead;
-        int current = 0;
-        FileInputStream fis1 = null, fis2 = null;
-        FileOutputStream fos1 = null, fos2 = null;
-        BufferedInputStream bis1 = null, bis2 = null;
-        BufferedOutputStream bos1 = null, bos2 = null;
-        OutputStream os1 = null, os2 = null;
-        InputStream is1 = null, is2 = null;
-        ServerSocket servsock = null;
-        Socket sock = null;
 
         try {
-            servsock = new ServerSocket(SOCKET);
+
+            ServerSocket ssoc = new ServerSocket(PORTO);
+            Socket sock = ssoc.accept();
+
+            DataInputStream din = new DataInputStream(sock.getInputStream());
+            DataOutputStream dout = new DataOutputStream(sock.getOutputStream());
+
+            String msgin = "", msgout = "";
+
+            File pasta = new File("C:/ftp/");
+            File[] listaDeFicheiros = pasta.listFiles();
+
+            String result;
+            int i = 0;
+
             while (true) {
-                System.out.println("Em espera...");
-                try {
-                    sock = servsock.accept();
-                    System.out.println("Conecção aceite : " + sock + "\n");
-                    
-                    //Listagem de ficheiros
-                    File pasta = new File("C:/ftp/");
-                    File[] listaDeFicheiros = pasta.listFiles();
-                    
-                    //conversão do array em ficheiro
-                    
-                    outfile = new PrintWriter(new FileWriter("C:/ftp/outputfile.txt"));
-                    
-                    if (listaDeFicheiros.length > 0) {
-                        StringBuilder sb = new StringBuilder();
+                if (listaDeFicheiros.length > 0) {
+                    StringBuilder sb = new StringBuilder();
 
-                        for (File file : listaDeFicheiros) {
-                            if (file.isFile()) {
-                                outfile.print(listaDeFicheiros[i]);
-                            }
+                    for (File file : listaDeFicheiros) {
+                        if (file.isFile()) {
+                            sb.append(i).append(file).append(",");
+                            i++;
                         }
-                        outfile.close();
                     }
-                    
-                    File arrayFile = new File("C:/ftp/outputfile.txt");
-                    byte[] bytearray = new byte[(int) arrayFile.length()];
-                    fis1 = new FileInputStream(arrayFile);
-                    bis1 = new BufferedInputStream(fis1);
-                    bis1.read(bytearray, 0, bytearray.length);
-                    os1 = sock.getOutputStream();
-                    System.out.println("A enviar o array...");
-                    os1.write(bytearray, 0, bytearray.length);
-                    os1.flush();
-                    System.out.println("Feito.");
-                    System.out.println("À espera do cliente...");
-                    
-                    //recebe o commando
-                    
-                    byte[] bytearray2 = new byte[1024];
-                    is2 = sock.getInputStream();
-                    fos2 = new FileOutputStream("C:/log.txt");
-                    bos2 = new BufferedOutputStream(fos2);
-                    bytesRead = is2.read(bytearray2, 0, bytearray2.length);
-                    current = bytesRead;
 
-                    do {
-                        bytesRead = is2.read(bytearray2, current, (bytearray2.length - current));
-                        if (bytesRead >= 0) {
-                            current += bytesRead;
-                        }
-                    } while (bytesRead > -1);
+                    msgout = sb.deleteCharAt(sb.length() - 1).toString();
+                }
 
-                    bos2.write(bytearray2, 0, current);
-                    bos2.flush();
-                    System.out.println("Comando recebido");
-                    
-                    String line;
-                    while((line = in.readLine()) != null)
-                    {
-                        int num = Integer.parseInt(line);
-                        FILE_TO_SEND = "C:/ftp/"+listaDeFicheiros[num];
-                    }
-                    in.close();
+                dout.writeUTF(msgout);
+                dout.flush();
+                msgin = din.readUTF();
+                System.out.println(msgin);
+                String fil = "";
 
-                    // enviar o ficheiro
-                    File myFile = new File(FILE_TO_SEND);
-                    byte[] mybytearray = new byte[(int) myFile.length()];
-                    fis2 = new FileInputStream(myFile);
-                    bis2 = new BufferedInputStream(fis2);
-                    bis2.read(mybytearray, 0, mybytearray.length);
-                    os2 = sock.getOutputStream();
-                    System.out.println("A enviar " + FILE_TO_SEND + "(" + mybytearray.length + " bytes)");
-                    os2.write(mybytearray, 0, mybytearray.length);
-                    os2.flush();
-                    System.out.println("Feito.");
-
-                } finally {
-                    if (bis1 != null && bis2 != null) {
-                        bis1.close();
-                        bis2.close();
-                    }
-                    if (os1 != null && os2 != null) {
-                        os1.close();
-                        os2.close();
-                    }
-                    if (sock != null) {
-                        sock.close();
+                for (File file : listaDeFicheiros) {
+                    if (file.toString() == listaDeFicheiros[Integer.parseInt(msgin)].toString()) {
+                        fil = file.toString();
                     }
                 }
+
+                File myfile = new File(fil);
+
+                byte[] mybytearray = new byte[(int) myfile.length()];
+                BufferedInputStream bis = new BufferedInputStream(new FileInputStream(myfile));
+                bis.read(mybytearray, 0, mybytearray.length);
+                OutputStream os = sock.getOutputStream();
+                os.write(mybytearray, 0, mybytearray.length);
+                os.flush();
+                sock.close();
+
             }
-        } finally {
-            if (servsock != null) {
-                servsock.close();
-            }
+
+        } catch (Exception saranvi) {
+
+            System.out.print(saranvi);
+
         }
-        
-        
+
     }
+
 }
