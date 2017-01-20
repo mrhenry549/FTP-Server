40902aad6331611c7d5eb880f9652c79f84861cd
Henrique <Henrique@Henrique-PC>
2017-01-20 14:07:52 +0000

    rever código

--- FTPServer/src/main/java/pt/atec/ftpserver/Main.java
+++ FTPServer/src/main/java/pt/atec/ftpserver/Main.java
@@ -9,6 +9,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         ServerModule s = new ServerModule();
         //ClientModule c = new ClientModule();
+        
+        
+        //http://stackoverflow.com/questions/16707816/ftp-client-server-model-for-file-transfer-in-java
     }
 
 }
